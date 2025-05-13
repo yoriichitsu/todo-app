fb862e5e4500ec0955e9d48833930df0b387555f
Ali Askari <aliak47.1382@gmail.com>
2025-05-13 16:08:26 +0330

    Update app.js
    برای استفاده داخل vm

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors'); // برای حل مشکل کور اضافه کردم
+const path = require('path');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 mongoose.connect('mongodb://mongo:27017/todos', {
   useNewUrlParser: true,
